7a390a65e11223006b8ed2fe3a29f4094418b7d2
adekfasha57-ui <adekfasha57@gmail.com>
2026-05-20 14:13:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,13 @@
-from pyrogram import Client, filters
+data = [
+    "apel",
+    "mangga",
+    "jeruk",
+    "melon"
+]
 
-# =========================
-# DATA TELEGRAM API
-# =========================
+cari = input("Cari buah: ")
 
-API_ID = 123456
-API_HASH = "ISI_API_HASH"
-BOT_TOKEN = "ISI_BOT_TOKEN"
+for item in data:
 
-# =========================
-# MEMBUAT BOT
-# =========================
-
-app = Client(
-    "botku",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
-
-# =========================
-# SAAT ADA PESAN MASUK
-# =========================
-
-@app.on_message(filters.text)
-async def balas_pesan(client, message):
-
-    teks = message.text.lower()
-
-    print("Pesan masuk:", teks)
-
-    # SEARCHING KATA
-    if "halo" in teks:
-        await message.reply("Hai juga 👋")
-
-    elif "makan" in teks:
-        await message.reply("Kamu lapar ya 🍜")
-
-    elif "siapa kamu" in teks:
-        await message.reply("Aku bot Telegram kecil 🤖")
-
-    else:
-        await message.reply("Aku tidak mengerti 😭")
-
-# =========================
-# MENJALANKAN BOT
-# =========================
-
-print("Bot sedang berjalan...")
-
-app.run()
+    if cari.lower() in item.lower():
+        print("Ketemu:", item)
